d353c7922d7e16c989d6e8feba0bdd06d6727dac
Ruth Lim <110249543+ruth-lim@users.noreply.github.com>
2024-11-10 11:53:18 +0800

    Added cors for request service

--- matching-service/request-service/index.ts
+++ matching-service/request-service/index.ts
@@ -1,13 +1,35 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from "express";
 import router from "./routes";
 import cors from "cors";
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  })
+);
+
+app.use((req: Request, res: Response, next: NextFunction): void => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
+    res.status(200).json({});
+    return;
+  }
+  next();
+});
+
+app.use(express.json());
 app.use(router);
 
 app.listen(3002, () => {
-    console.log("Server started on port 3002");
-})
+  console.log("Server started on port 3002");
+});
